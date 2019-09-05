448b3d3f2a11df5bbc0ea92a12d363d841c25d16
wiseCyril <unnamadim@getcarbon.co>
2019-09-05 06:19:33 +0100

    DOM testing using jsdom

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,22 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
 
 describe ('our first test', function() {
   it('should pass', function() {
     expect(true).to.equal(true);
   });
 });
+
+describe('index.html', function() {
+  it('should say hello', function() {
+    const index = fs.readFileSync('./src/index.html', 'utf-8');
+    jsdom.env(index, function(err, window) {
+      const h1 = window.document.getElementByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Development Server");
+      done();
+      window.close();
+    })
+  })
+})
